2b0cc8b32852fa2cf80aa29c854fdf8c43fd4249
Filip <filipmarkovic@gmail.com>
2022-08-15 19:08:16 +0200

    .

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/api/v1/survey', require('./routes/api/v1/survey'))
+app.use("/api/v1/survey", require("./routes/api/v1/survey"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
 
 app.listen(PORT)
